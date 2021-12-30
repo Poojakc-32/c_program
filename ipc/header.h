5481dd87ca75cea186b17e09b496aaa28b773381
Poojakc-32 <96101018+Poojakc-32@users.noreply.github.com>
2021-12-30 13:15:31 +0530

    Add files via upload

---
+++ ipc/header.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
